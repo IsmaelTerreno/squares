ed0dd8445fd8c63e0d2633772bc364e00e13f9a4
Ismael Terreno <iterreno@zigatta.com>
2018-09-29 23:05:10 -0300

    Config env.

--- src/config/backend.api.config.js
+++ src/config/backend.api.config.js
@@ -1,3 +1,3 @@
 export default {
-  urlBackend: process.env.BACKEND_API_HOST_URL || "https://squares-api-demo.herokuapp.com",
+  urlBackend: process.env.BACKEND_API_HOST_URL || "https://squares-api-demo.herokuapp.com:4000/api/v1/squares",
 };
